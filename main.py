efd1a0de9bb556f6ab6e6ae098b89dffae1c6c92
felix <felix@reseau-65.lycee-ledantec.ac-rennes.fr>
2020-02-05 17:23:19 +0100

    V4 -> Utilisation du tag de la bdd + modification de NULL, a 0 a 1

--- main.py
+++ main.py
@@ -1,56 +1,72 @@
 import mysql.connector
-import requests
 import json
 from icinga2api.client import Client
 from icinga2 import Icinga2API
 
-from requests.packages.urllib3.exceptions import InsecureRequestWarning
-requests.packages.urllib3.disable_warnings(InsecureRequestWarning) #Suppression des warnings lié au fait que l'on est un certificat ssl non valide 
 
-def connectobdd():
+db = mysql.connector.connect(host="127.0.0.1", user="felix", password="felix22", database="srtest2") #connexion a la base de donnée 
+req=db.cursor()
+client = Client('https://172.20.20.73:5665', 'felix', 'felix22') #connexion a l'API REST
 
-    db = mysql.connector.connect(host="127.0.0.1", user="felix", password="felix22", database="srtest2") #connexion a la base de donnée 
-    req=db.cursor() #Creation d'une variable pour envoyer la requete a la BDD
-    req.execute("SELECT *  FROM switches INNER JOIN configswitches ON switches.ConfigSwitchId = configswitches.ConfigSwitchId INNER JOIN modeles ON switches.ModeleId = modeles.ModeleId") # Requete envoyer a la BDD
+def infoBdd():
+
+    req.execute("SELECT * FROM switches INNER JOIN configswitches ON switches.ConfigSwitchId = configswitches.ConfigSwitchId INNER JOIN modeles ON switches.ModeleId = modeles.ModeleId") # Requete envoyer a la BDD
     reponce= req.fetchall() #Recuperation de tous les information qu'il a pus trouver a la suite de la requete 
 
     for row in reponce:
 
-        ip = row[8] #Recuperation de la colone 8 avec l'ip
-        switch = row[11]+"_"+row[10] #Recuperation de la colone 11 et 10 contenant la marque et le nom du switch
-
+        idSwitch = row[1]
+        ip = row[10] #Recuperation de la colone 8 avec l'ip
+        switch = row[13]+"_"+row[12] #Recuperation de la colone 11 et 10 contenant la marque et le nom du switch
+        tag=row[7] #Recuperation de l'information tag
         print("Information recuperer dans le base de donnée : ")
+        print("Id Du Switch : "+idSwitch)
         print("Ip : "+ip)
         print("Switch : "+switch)
-        verifier(switch, ip) #Verification si le switch existe deja sur l'hyperviseur !
+        print("Tag : "+str(tag))
+        verifier(switch, ip, tag, idSwitch) #Verification si le switch existe deja sur l'hyperviseur !
         
 
-    db.close()
+    
 
-def addHost(ip, switch):
+def addHost(ip, switch, idSwitch):
 
-    client = Client('https://172.20.20.73:5665', 'felix', 'felix22') #connexion a l'API REST
+    
     client.objects.create('Host', switch, ['generic-host'], {'address': ip}) #Ajout d'un d'un switch avec les information recuperer dans la BDD
+    req.execute("UPDATE switches SET tag='0' WHERE SwitchId="+idSwitch+";")
 
-
-
-
-
-
-def verifier(switch, ip):
-
-    api = Icinga2API(username="felix", password="felix22", url="https://172.20.20.73:5665") #Connexion a l'API REST
+def verifier(switch, ip, tag, idSwitch):
     
-    if api.hosts.exists(switch) == True: #ON verifie sur le switch existe grace a l'api qui nous repondra True ou False
-        print("[Iconga2] - Le Switch Existe Déja") 
-        print("===============================================================")
-        pass # Si c'est true on ne fait rien
-    else:
-        print("[Icinga2] - Le Switch N'existe Pas")
-        print("===============================================================")
-        addHost(ip, switch) #sinon on envoie les infos a la fonction ADDHOST !
+    if tag == None: #Si tag est a null cela veut dire qu'il est nouveau et qu'il faut donc le crée
+        addHost(ip, switch, idSwitch)
+    elif tag == 0: #Si tag est  a 0 il n'a pas ete modifier rien a faire
+        print("Le Switch n'a pas été modifier")
+        print("=================================")
+        pass
+    elif tag == 1: # Si Tag est a 1 le switch est a modifier sur icinga2 
+        print("Le Switch a été modifier")
+        print("=================================")
+        update(switch,ip,tag)
+        
+def update(switch,ip,tag):
+    
+    client.objects.update(
+        'Host',
+        switch,
+        {'address': ip}
+    )
+
+
+
+def relationSwitch():
+    
+    req.execute("SELECT")
+
 
     
+
+    
+
 
 if __name__ == "__main__":
-    connectobdd()
+    infoBdd()
